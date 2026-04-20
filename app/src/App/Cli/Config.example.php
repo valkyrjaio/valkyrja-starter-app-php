96b84790e203bbbda46684573deff66209dfd98b
Melech Mizrachi <melechmizrachi@gmail.com>
2026-04-19 21:34:32 -0700

    [Config] Simplify Config constructors and fix package header.

--- app/src/App/Cli/Config.example.php
+++ app/src/App/Cli/Config.example.php
@@ -5,7 +5,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the Valkyrja Application package.
+ * This file is part of the Valkyrja Framework package.
  *
  * (c) Melech Mizrachi <melechmizrachi@gmail.com>
  *
@@ -18,64 +18,33 @@ namespace App\Cli;
 use App\Cli\Provider\ComponentProvider;
 use App\Http\Config as AppHttpConfig;
 use Valkyrja\Application\Data\CliConfig;
-use Valkyrja\Application\Data\HttpConfig;
-use Valkyrja\Application\Kernel\Contract\ApplicationContract;
 use Valkyrja\Application\Provider\CliWithHttpApplicationComponentProvider;
-use Valkyrja\Application\Provider\Contract\ComponentProviderContract;
 use Valkyrja\Cli\Server\Constant\CommandName;
 
 final class Config extends CliConfig
 {
-    /**
-     * @param non-empty-string                          $namespace
-     * @param non-empty-string                          $dir
-     * @param non-empty-string                          $version
-     * @param non-empty-string                          $environment
-     * @param non-empty-string                          $timezone
-     * @param non-empty-string                          $key
-     * @param non-empty-string                          $dataPath
-     * @param non-empty-string                          $dataNamespace
-     * @param non-empty-string                          $applicationName
-     * @param non-empty-string                          $defaultCommandName
-     * @param class-string<ComponentProviderContract>[] $providers
-     * @param array<callable(ApplicationContract):void> $callbacks
-     */
-    public function __construct(
-        string $namespace = 'App',
-        string $dir = __DIR__ . '/../../..',
-        string $version = '1.0.0',
-        string $environment = 'production',
-        bool $debugMode = true,
-        string $timezone = 'UTC',
-        string $key = 'some_secret_app_key',
-        string $dataPath = 'App/Cli/Data',
-        string $dataNamespace = 'App\\Cli\\Data',
-        string $applicationName = 'cli',
-        string $defaultCommandName = CommandName::LIST,
-        array $providers = [
-            CliWithHttpApplicationComponentProvider::class,
-            ComponentProvider::class,
-        ],
-        array $callbacks = [
-            [ComponentProvider::class, 'publish'],
-        ],
-        HttpConfig $http = new AppHttpConfig(),
-    ) {
+    public function __construct()
+    {
         parent::__construct(
-            namespace: $namespace,
-            dir: $dir,
-            version: $version,
-            environment: $environment,
-            debugMode: $debugMode,
-            timezone: $timezone,
-            key: $key,
-            dataPath: $dataPath,
-            dataNamespace: $dataNamespace,
-            applicationName: $applicationName,
-            defaultCommandName: $defaultCommandName,
-            providers: $providers,
-            callbacks: $callbacks,
-            http: $http,
+            namespace: 'App',
+            dir: __DIR__ . '/../../..',
+            version: '1.0.0',
+            environment: 'production',
+            debugMode: true,
+            timezone: 'UTC',
+            key: 'some_secret_app_key',
+            dataPath: 'App/Cli/Data',
+            dataNamespace: 'App\\Cli\\Data',
+            applicationName: 'cli',
+            defaultCommandName: CommandName::LIST,
+            providers: [
+                CliWithHttpApplicationComponentProvider::class,
+                ComponentProvider::class,
+            ],
+            callbacks: [
+                [ComponentProvider::class, 'publish'],
+            ],
+            http: new AppHttpConfig(),
         );
     }
 }
--- app/src/App/Http/Config.example.php
+++ app/src/App/Http/Config.example.php
@@ -5,7 +5,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the Valkyrja Application package.
+ * This file is part of the Valkyrja Framework package.
  *
  * (c) Melech Mizrachi <melechmizrachi@gmail.com>
  *
@@ -17,79 +17,33 @@ namespace App\Http;
 
 use App\Http\Provider\ComponentProvider;
 use Valkyrja\Application\Data\HttpConfig;
-use Valkyrja\Application\Kernel\Contract\ApplicationContract;
-use Valkyrja\Application\Provider\Contract\ComponentProviderContract;
 use Valkyrja\Application\Provider\HttpApplicationComponentProvider;
-use Valkyrja\Http\Middleware\Contract\RequestReceivedMiddlewareContract;
-use Valkyrja\Http\Middleware\Contract\RouteDispatchedMiddlewareContract;
-use Valkyrja\Http\Middleware\Contract\RouteMatchedMiddlewareContract;
-use Valkyrja\Http\Middleware\Contract\RouteNotMatchedMiddlewareContract;
-use Valkyrja\Http\Middleware\Contract\SendingResponseMiddlewareContract;
-use Valkyrja\Http\Middleware\Contract\TerminatedMiddlewareContract;
-use Valkyrja\Http\Middleware\Contract\ThrowableCaughtMiddlewareContract;
-use Valkyrja\Http\Middleware\Data\Contract\ConfigContract;
 use Valkyrja\Http\Server\Middleware\CacheResponseMiddleware;
 
-final class Config extends HttpConfig implements ConfigContract
+final class Config extends HttpConfig
 {
-    /**
-     * @param non-empty-string                                  $namespace
-     * @param non-empty-string                                  $dir
-     * @param non-empty-string                                  $version
-     * @param non-empty-string                                  $environment
-     * @param non-empty-string                                  $timezone
-     * @param non-empty-string                                  $key
-     * @param non-empty-string                                  $dataPath
-     * @param non-empty-string                                  $dataNamespace
-     * @param class-string<ComponentProviderContract>[]         $providers
-     * @param array<callable(ApplicationContract):void>         $callbacks
-     * @param class-string<RequestReceivedMiddlewareContract>[] $requestReceivedMiddleware
-     * @param class-string<RouteMatchedMiddlewareContract>[]    $routeMatchedMiddleware
-     * @param class-string<RouteNotMatchedMiddlewareContract>[] $routeNotMatchedMiddleware
-     * @param class-string<RouteDispatchedMiddlewareContract>[] $routeDispatchedMiddleware
-     * @param class-string<ThrowableCaughtMiddlewareContract>[] $throwableCaughtMiddleware
-     * @param class-string<SendingResponseMiddlewareContract>[] $sendingResponseMiddleware
-     * @param class-string<TerminatedMiddlewareContract>[]      $terminatedMiddleware
-     */
-    public function __construct(
-        string $namespace = 'App',
-        string $dir = __DIR__ . '/../../..',
-        string $version = '1.0.0',
-        string $environment = 'production',
-        bool $debugMode = true,
-        string $timezone = 'UTC',
-        string $key = 'some_secret_app_key',
-        string $dataPath = 'App/Http/Data',
-        string $dataNamespace = 'App\\Http\\Data',
-        array $providers = [
-            HttpApplicationComponentProvider::class,
-            ComponentProvider::class,
-        ],
-        array $callbacks = [
-            [ComponentProvider::class, 'publish'],
-        ],
-        public array $requestReceivedMiddleware = [
-            CacheResponseMiddleware::class,
-        ],
-        public array $routeMatchedMiddleware = [],
-        public array $routeNotMatchedMiddleware = [],
-        public array $routeDispatchedMiddleware = [],
-        public array $throwableCaughtMiddleware = [],
-        public array $sendingResponseMiddleware = [],
-        public array $terminatedMiddleware = [],
-    ) {
+    public function __construct()
+    {
         parent::__construct(
-            namespace: $namespace,
-            dir: $dir,
-            version: $version,
-            environment: $environment,
-            debugMode: $debugMode,
-            timezone: $timezone,
-            key: $key,
-            dataPath: $dataPath,
-            dataNamespace: $dataNamespace,
-            providers: $providers,
-            callbacks: $callbacks,
+            namespace: 'App',
+            dir: __DIR__ . '/../../..',
+            version: '1.0.0',
+            environment: 'production',
+            debugMode: true,
+            timezone: 'UTC',
+            key: 'some_secret_app_key',
+            dataPath: 'App/Http/Data',
+            dataNamespace: 'App\\Http\\Data',
+            providers: [
+                HttpApplicationComponentProvider::class,
+                ComponentProvider::class,
+            ],
+            callbacks: [
+                [ComponentProvider::class, 'publish'],
+            ],
+            requestReceivedMiddleware: [
+                CacheResponseMiddleware::class,
+            ],
         );
     }
 }
