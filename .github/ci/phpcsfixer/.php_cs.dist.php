59885548aeb767831c7cb19b6e94d5d585b7430d
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-02 16:00:29 -0700

    [PhpCsFixer] refactor: Build the copyright header from the package name.

--- .github/ci/phpcsfixer/.php_cs.dist.php
+++ .github/ci/phpcsfixer/.php_cs.dist.php
@@ -13,14 +13,6 @@ declare(strict_types=1);
 use PhpCsFixer\Finder;
 use Valkyrja\Fixer\Rules;
 
-$header = <<<EOF
-    This file is part of the Valkyrja Application package.
-
-    Copyright (c) 2016-present Melech Mizrachi
-
-    Released under the MIT License. See LICENSE.md for details.
-    EOF;
-
 $finder = Finder::create()
     // Finder ignores a dot directory by default, which put every PHP file under
     // .github outside the header rule. Those files are this repository's own source
@@ -35,4 +27,4 @@ $finder = Finder::create()
     ->exclude('vendor')
     ->in(__DIR__ . '/../../../');
 
-return Rules::getConfig($finder, $header);
+return Rules::getConfig($finder, 'Valkyrja Application');
